17c45a6fdaf7237180ba87bb132ab1d6de7cdcb9
AriSai <arisainft@gmail.com>
2026-03-09 21:52:14 +0100

    add computed_at timestamp to TPI

--- api/tpi.py
+++ api/tpi.py
@@ -781,7 +781,6 @@ def compute_all_indicators():
         result["ltpi_error"] = str(e)
 
     return result
-    import datetime
     result["computed_at"] = time.strftime("%Y-%m-%d %H:%M UTC", time.gmtime())
 
     return result
